cf65a2851c86499442b93aaa8cf012f043b401cb
Ben Merken <ben.merken@gmail.com>
2024-01-05 11:33:41 +0100

    Remap half-page jumps to home-row keys

--- lua/ben/core/remap.lua
+++ lua/ben/core/remap.lua
@@ -9,8 +9,8 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>+2<CR>gv=gv")
 
 -- Keep cursor in middle of the screen during half-page jumps and searches
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-j>", "<C-d>zz")
+vim.keymap.set("n", "<C-k>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
