bba6357c3d8a53e32d72c92ca85c131ce028fb4a
Ben Merken <ben.merken@gmail.com>
2024-01-10 21:48:15 +0100

    Add 'select all' remap

--- lua/ben/core/remap.lua
+++ lua/ben/core/remap.lua
@@ -1,18 +1,23 @@
+local keymap = vim.keymap
+
 vim.g.mapleader = " "
 -- Format all, and return cursor to last position before formatting
-vim.keymap.set("n", "<leader>fa", "ggVG=<c-o><c-0>")
+keymap.set("n", "<leader>fa", "ggVG=<c-o><c-0>")
 -- Go to file explorer
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Move highlighted text around
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '>+2<CR>gv=gv")
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '>+2<CR>gv=gv")
 
 -- Keep cursor in middle of the screen during half-page jumps and searches
-vim.keymap.set("n", "<C-i>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+keymap.set("n", "<C-i>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
 
 -- Keep previous pasted buffer
-vim.keymap.set("n", "<leader>p", "\"_dP")
+keymap.set("n", "<leader>p", "\"_dP")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
