0083ae93372a043513858d0650f7a36f7e846fa5
Ben Merken <ben.merken@gmail.com>
2024-12-21 18:32:57 +0100

    Update lazy packages, and rename TypeScript language server

--- lua/ben/plugins/lsp/nvim-lspconfig.lua
+++ lua/ben/plugins/lsp/nvim-lspconfig.lua
@@ -64,7 +64,7 @@ return {
         })
 
         -- TypeScript
-        lspconfig["tsserver"].setup({
+        lspconfig["ts_ls"].setup({
             capabilities = capabilities,
             on_attach = on_attach,
         })
